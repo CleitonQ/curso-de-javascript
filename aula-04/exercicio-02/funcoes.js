7827fbc89a7964822dfa2d584fcee1bba0e75df1
CleitonQ <cleitonqueirozc@gmail.com>
2025-02-21 05:22:12 -0300

    Update 21022025 1

---
+++ aula-04/exercicio-02/funcoes.js
@@ -0,0 +1,12 @@
+// Exportamos a função
+export function dobro(numero) {
+    return numero * 2;
+}
+
+export function somar(numero1, numero2){
+    return numero1 + numero2;
+}
+
+export function subtrair(numero1, numero2){
+    return numero1 - numero2;
+}
